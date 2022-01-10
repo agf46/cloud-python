ba75242197a9c898d9ee81f3ddefa47b1768caeb
agf46 <akshaygf@gmail.com>
2022-01-10 10:31:40 -0800

    Add encryption listing

--- s3Ops.py
+++ s3Ops.py
@@ -1,10 +1,11 @@
 # Simply python script to dump list of S3 buckets to a CSV file
 # Boto3 documentation: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-example-creating-buckets.html
 # To do:
-# - Add Error handling with botocore if we want to get really fancy with it
+# Make this a CLI operation
 
 import csv
 import boto3
+from botocore.exceptions import ClientError 
 
 # Init boto3 resource for S3
 # NOTE: The session is created by using creds available under ~/.aws/credentials
@@ -28,11 +29,22 @@ def writeBuckets2csv():
             s3listing = bucket["Name"]
             f.write(s3listing + '\n')
 
+def listBucketsWithEncryption():
+    for bucket in response['Buckets']:
+        try: 
+            is_enc = s3.get_bucket_encryption(Bucket=bucket['Name'])
+            rules = is_enc['ServerSideEncryptionConfiguration']['Rules']
+            print('Bucket: %s, Encryption: %s' % (bucket['Name']))
+        except ClientError as e: 
+            if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
+                print('Bucket: %s, no server-side encryption' % (bucket['Name']))
+            else: 
+                print("Bucket: %s, error encountered: %s" % (bucket['Name'], e))
 
 def main():
     listS3buckets()
     writeBuckets2csv()
-
+    listBucketsWithEncryption()
 
 if __name__ == "__main__":
     main()
